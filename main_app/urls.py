2f0cb1783ab3148c24be1c7ee6c6126412260e18
Joma121 <martinsjoshua1986@gmail.com>
2021-02-07 21:05:05 -0800

    removes unneeded account paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,8 +6,5 @@ urlpatterns = [
     path('characters/', views.all_characters, name="all_characters"),
     path('characters/my', views.my_characters, name="my_characters"),
     path('characters/new', views.new_character, name="new_character"),
-    path('account/signup', views.signup, name="signup"),
-    path('account/login', views.login, name="login"),
-    path('account/logout', views.logout, name="logout"),
 
 ]

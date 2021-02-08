098a1223a1b62b9a164b6a3b68bce2ebbdbcf1fa
Joma121 <martinsjoshua1986@gmail.com>
2021-02-07 21:08:19 -0800

    adds model to confirm logout, logic to run modal, and styling

--- main_app/static/app.js
+++ main_app/static/app.js
@@ -2,3 +2,11 @@ $('#mobile-nav').click(function(event){
     $('.navbar-menu').toggleClass('is-active');
     $('.navbar-burger').toggleClass('is-active');
 })
+
+$('#open-logout-modal').click(function(event){
+    $('#logout-modal').toggleClass('is-active');
+})
+
+$('.close-logout-modal').click(function(event){
+    $('#logout-modal').toggleClass('is-active');
+})

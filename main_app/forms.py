76b15cc2f808b1d8c1fbc9294cbe3f7bcea7883c
Joma121 <martinsjoshua1986@gmail.com>
2021-02-11 00:25:33 -0800

    adds form class for character creator

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+from .models import CustomUser, Character
 
 class CustomUserCreationForm(UserCreationForm):
 
@@ -13,3 +13,8 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email')
+
+class CharacterCreationForm(forms.ModelForm):
+    class Meta:
+        model = Character
+        fields = ('name', 'level', 'alignment', 'lifestyle', 'hair', 'skin', 'eyes', 'height', 'weight', 'age', 'notes', 'strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma', 'trainer_license', 'pokedex', 'pokeballs', 'potions', 'pokedollars', 'pack', 'race', 'trainer_class', 'specialization', 'path', 'starter_pokemon', 'user')

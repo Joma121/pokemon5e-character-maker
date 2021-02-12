f96e278da5fb9e5a24a6cfa8927d49abb2329675
Joma121 <martinsjoshua1986@gmail.com>
2021-02-12 00:11:14 -0800

    adds delete path and view, modifications to edit, new and show views

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('characters/', views.all_characters, name="all_characters"),
-    path('characters/my', views.my_characters, name="my_characters"),
+    path('characters/', views.my_characters, name="my_characters"),
     path('characters/new', views.new_character, name="new_character"),
+    path('characters/edit/<int:char_id>', views.edit_character, name="edit_character"),
+    path('characters/delete/<int:char_id>', views.delete_character, name="delete_character"),
+    path('characters/<int:char_id>', views.show_character, name="show_character"),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,63 +1,101 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import CustomUser, Ability, Skill, Language, PokemonType, Move, RaceFeature, ClassFeature, PokemonAbility, Race, TrainerClass, Specialization, TrainerPath, Pokemon, Character, EggGroup
+from .models import CustomUser, Ability, Skill, Language, PokemonType, Move, RaceFeature, ClassFeature, PokemonAbility, Race, TrainerClass, Specialization, Pokemon, Character, EggGroup
 from .forms import CustomUserCreationForm, CustomUserChangeForm, CharacterCreationForm
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
-def all_characters(request):
-    return render(request, 'characters/all_characters.html')
-
 @login_required
 def my_characters(request):
-    return render(request, 'characters/my_characters.html')
+    characters = Character.objects.filter(user_id=request.user.id)
+    context = {'characters': characters}
+    return render(request, 'characters/my_characters.html', context)
 
 @login_required
 def new_character(request):
     user = CustomUser.objects.get(id=request.user.id)
+    trainer_class = TrainerClass.objects.get(id=1)
+    level = 1
     if request.method == 'POST':
-        character_form = CharacterCreationForm(request.POST, request.FILES)
+        character_form = CharacterCreationForm(request.POST)
+        print(character_form)
         if character_form.is_valid():
-            new_character = character_form.save(commit=false)
+            new_character = character_form.save(commit=False)
             new_character.user = user
+            new_character.level = level
+            new_character.trainer_class = trainer_class
+            new_character.save()
+            return redirect('my_characters')
 
     character_form = CharacterCreationForm()
-    languages = Language.objects.all()
-    abilities = Ability.objects.all()
-    skills = Skill.objects.all()
-
     races = Race.objects.select_related()
     raceFeatures = RaceFeature.objects.select_related()
-
-    trainer = TrainerClass.objects.get(id=1)
     trainerFeatures = ClassFeature.objects.all()
-    specializations = Specialization.objects.prefetch_related('skill').select_related('pokemon_type')
-    trainerPaths = TrainerPath.objects.all()
-
-    pokemonTypes = PokemonType.objects.all()
-    pokemonAbilities = PokemonAbility.objects.all()
-    eggGroups = EggGroup.objects.all()
+    specializations = Specialization.objects.all()
     pokemon = Pokemon.objects.all()
-
+    
     context = {
-        'languages': languages,
-        'abilities': abilities,
-        'skills': skills,
         'races': races,
         'raceFeatures': raceFeatures,
-        'trainer': trainer,
         'trainerFeatures': trainerFeatures,
-        'trainerPaths': trainerPaths,
         'specializations': specializations,
-        'pokemonTypes': pokemonTypes,
-        'pokemonAbilities': pokemonAbilities,
-        'eggGroups': eggGroups,
         'pokemons': pokemon,
         'character_form': character_form,
     }
-    print (trainer)
     return render(request, 'characters/new_character.html', context)
-    
+
+def show_character(request, char_id):
+    character = Character.objects.get(id=char_id)
+    race = Race.objects.get(id=character.race.id)
+    racefeatures = RaceFeature.objects.select_related('race')
+    trainerfeatures = ClassFeature.objects.all()
+    languages = Language.objects.all()
+    specializations = Specialization.objects.all()
+    pokemon = Pokemon.objects.get(id=character.starter_pokemon.id)
+
+    context = {
+        'character': character,
+        'race': race,
+        'racefeatures': racefeatures,
+        'trainerfeatures': trainerfeatures,
+        'languages': languages,
+        'specializations': specializations,
+        'starter': pokemon,
+    }
+    return render(request, 'characters/show_character.html', context)
+
+def edit_character(request, char_id):
+    character = Character.objects.get(id=char_id)
+
+    if request.method == 'POST':
+        character_form = CharacterCreationForm(request.POST, instance=character)
+        if character_form.is_valid():
+            character_form.save()
+            return redirect('my_characters')
+
+    character_form = CharacterCreationForm(instance=character)
+    races = Race.objects.select_related()
+    raceFeatures = RaceFeature.objects.select_related()
+    trainerFeatures = ClassFeature.objects.all()
+    specializations = Specialization.objects.all()
+    pokemon = Pokemon.objects.all()
+    if character_form.errors:
+        print(character_form.errors)
+    context = {
+        'races': races,
+        'raceFeatures': raceFeatures,
+        'trainerFeatures': trainerFeatures,
+        'specializations': specializations,
+        'pokemons': pokemon,
+        'character_form': character_form,
+        'character': character,
+    }
+    return render(request, 'characters/edit_character.html', context)
+
+def delete_character(request, char_id):
+    character = Character.objects.get(id=char_id)
+    character.delete()
+    return redirect("my_characters")

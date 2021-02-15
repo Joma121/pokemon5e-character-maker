a1510e2a970c3ebd2b3db4a2a923a09375701824
Joma121 <martinsjoshua1986@gmail.com>
2021-02-15 10:17:18 -0800

    removed unused code

--- main_app/static/app.js
+++ main_app/static/app.js
@@ -24,7 +24,6 @@ $('.close-logout-modal').click(function(event){
 $('#id_login').addClass("input");
 $("#id_login").parent("p").addClass("block control");
 $('#id_login').prev("label").addClass("label");
-// $('#id_login').after("<span class='icon is-small is-left'><i class='fas fa-envelope'></i></span>");
 $("#id_login").change(function(event){
     const mailformat = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$";
     if(!this.value.match(mailformat)){
@@ -52,9 +51,7 @@ $("#id_login").change(function(event){
  */ 
 $("#id_email").addClass("input");
 $("#id_email").parent("p").addClass(`block control`);
-// $("#id_email").parent("p").wrap(`<div class="field"></div>`);
 $("#id_email").prev("label").addClass("label");
-// $('#id_email').after(`<span class="icon is-small is-left"><i class="fas fa-envelope"></i></span>`);
 $("#id_email").change(function(event){
     const mailformat = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$";
     if(!this.value.match(mailformat)){
@@ -84,7 +81,6 @@ $("#id_email").change(function(event){
 $("#id_password").addClass('input');
 $("#id_password").parent("p").addClass("control block");
 $("#id_password").prev("label").addClass("label");
-// $('#id_password').after("<span class='icon is-small is-left'><i class='fas fa-lock'></i></span>");
 $("#id_password").change(function(event){
     if(this.value.length < 8){
         if(!$(this).hasClass("is-warning")){
@@ -114,7 +110,6 @@ $("#id_password1").addClass('input');
 $("#id_password1").parent("p").addClass("control block");
 $("#id_password1").parent("p").wrap("<div class='field'></div>")
 $("#id_password1").prev("label").addClass("label");
-// $('#id_password1').after("<span class='icon is-left'><i class='fas fa-lock'></i></span>");
 $("#id_password1").change(function(event){
     if(this.value.length < 8){
         if(!$(this).hasClass("is-warning")){

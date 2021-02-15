823d68ad3cb8262e8deee80bac37fd449fbe3ac8
Joma121 <martinsjoshua1986@gmail.com>
2021-02-15 11:55:31 -0800

    add static_root to settings

--- pokemon5e_character_creator/settings.py
+++ pokemon5e_character_creator/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # config/settings.py
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
